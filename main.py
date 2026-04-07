2e44e16e9471b21e61f737c39121d2e0d1296d88
Jure-G <gorjup.jure91@gmail.com>
2026-04-07 08:52:19 +0200

    actually make it 60FPS

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     
-
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,8 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
         pygame.display.flip()
+        dt = clock.tick() / 1000
+        clock.tick(60)
 
 
 if __name__ == "__main__":
